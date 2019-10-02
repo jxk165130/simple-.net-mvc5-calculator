32eda4432f8dcbf6d71bcee233eebc85f379a8f0
Jinho Kim <40884275+jxk165130@users.noreply.github.com>
2019-10-01 21:42:28 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleWebCalculator.Models;
+
+namespace SimpleWebCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(Calculator calculator)
+        {
+           if(calculator.Operator == "+")
+            {
+                calculator.Result = calculator.FirstNumber + calculator.SecondNumber;
+            }
+           else if(calculator.Operator == "-")
+            {
+                calculator.Result = calculator.FirstNumber - calculator.SecondNumber;
+            }
+           else if(calculator.Operator == "*")
+            {
+                calculator.Result = calculator.FirstNumber * calculator.SecondNumber;
+            }
+            else
+            {
+                calculator.Result = calculator.FirstNumber / calculator.SecondNumber;
+            }
+
+            return View(calculator);
+        }
+        
+        
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleWebCalculator.Models
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public string Operator { get; set; }
+
+        public Double Result { get; set; }
+    }
+}
